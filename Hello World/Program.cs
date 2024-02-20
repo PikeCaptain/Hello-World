808523f93deb74d19c60f752c559c5220385f87d
PikeCaptain <idunno6418@gmail.com>
2024-02-20 13:25:14 -0600

    Initial commit

---
+++ Hello World/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+// MY collection of classes for this project.
+namespace HelloWorld_Landon_Simmons
+{
+    // Main method is executed when application starts
+    class Program
+    {
+        // The Main method is excuted when application starts
+        static void Main(string[] args)
+        {
+            // Asks the user for first name
+            Console.WriteLine("What is your first name?");
+            // Assigns user input to firstName variable
+            string firstName = Console.ReadLine();
+            // Asks the user for last name
+            Console.WriteLine("What is your last name?");
+            // Assigns the user input to lastName variable
+            string lastName = Console.ReadLine();
+            
+            // Prints a welcome message with firstName and lastName
+            Console.WriteLine("Hello, " + firstName + " " + lastName + "!");
+        }
+    }
+}
